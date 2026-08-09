b4c1802757567590cfe7bba1d0d0fdc0aebef70b
ImJamie <jamie@929design.co.uk>
2026-08-09 19:00:35 +0100

    Small Change

--- js/config.js
+++ js/config.js
@@ -19,5 +19,5 @@ export const outerRing = [
   [-180,-85]
 ];
 
-export const wsUri = `wss://api.hideandseek.makingthat.org/questions`;
+export const wsUri = `ws://api.hideandseek.makingthat.org:67/questions/map`;
 export const wsAuthUri = `ws://games.lunarpixel.uk:67/auth`;
--- js/input.js
+++ js/input.js
@@ -18,56 +18,56 @@ export function initInputs() {
    * @returns {string} A useful value.
    */
 
-  map.on('mousedown', (e) => {
+  // map.on('mousedown', (e) => {
+  //
+  //   // Check if the middle mouse button was pressed
+  //   if (e.originalEvent.button === 1) {
+  //     console.log('Middle click at coordinates:', e.lngLat);
+  //
+  //     inThermometerMode = true;
+  //
+  //     map.getCanvas().style.cursor = 'crosshair';
+  //   }
+  // });
 
-    // Check if the middle mouse button was pressed
-    if (e.originalEvent.button === 1) {
-      console.log('Middle click at coordinates:', e.lngLat);
+  // map.on( "click", (e) => {
+  //   if (!inThermometerMode) {
+  //
+  //   }
+  //   else {
+  //     if (e.originalEvent.button === 0 && !hasClicked ) {
+  //       console.log('clicked in thermometer mode at coordinates:', e.lngLat);
+  //       thermoPos1 = e.lngLat;
+  //       console.log(thermoPos1);
+  //       hasClicked = true;
+  //     }else if (e.originalEvent.button === 0 && hasClicked) {
+  //       console.log('clicked again in thermometer mode at coordinates:', e.lngLat);
+  //       thermoPos2 = e.lngLat;
+  //       console.log(thermoPos2);
+  //       map.getCanvas().style.cursor = '';
+  //       inThermometerMode = false;
+  //       hasClicked = false;
+  //
+  //       newThermometer(thermoPos1, thermoPos2, true);
+  //
+  //     }
+  //   }
+  // })
 
-      inThermometerMode = true;
-
-      map.getCanvas().style.cursor = 'crosshair';
-    }
-  });
-
-  map.on( "click", (e) => {
-    if (!inThermometerMode) {
-
-    }
-    else {
-      if (e.originalEvent.button === 0 && !hasClicked ) {
-        console.log('clicked in thermometer mode at coordinates:', e.lngLat);
-        thermoPos1 = e.lngLat;
-        console.log(thermoPos1);
-        hasClicked = true;
-      }else if (e.originalEvent.button === 0 && hasClicked) {
-        console.log('clicked again in thermometer mode at coordinates:', e.lngLat);
-        thermoPos2 = e.lngLat;
-        console.log(thermoPos2);
-        map.getCanvas().style.cursor = '';
-        inThermometerMode = false;
-        hasClicked = false;
-
-        newThermometer(thermoPos1, thermoPos2, true);
-
-      }
-    }
-  })
-
-  let lastMousePos = null;
-
-  map.on('mousemove', (e) => {
-    lastMousePos = e.point; // {x,y} screen pixel coords
-  });
-
-  document.addEventListener('keydown', (e) => {
-    if (!lastMousePos) return;
-
-    if (e.key === 'R') {
-      const lnglat = map.unproject(lastMousePos);
-      newCircleZone(lnglat.lng, lnglat.lat, 5, false);
-    }
-  })
+  // let lastMousePos = null;
+  //
+  // map.on('mousemove', (e) => {
+  //   lastMousePos = e.point; // {x,y} screen pixel coords
+  // });
+  //
+  // document.addEventListener('keydown', (e) => {
+  //   if (!lastMousePos) return;
+  //
+  //   if (e.key === 'R') {
+  //     const lnglat = map.unproject(lastMousePos);
+  //     newCircleZone(lnglat.lng, lnglat.lat, 5, false);
+  //   }
+  // })
 }
 
 //region UI Radar
